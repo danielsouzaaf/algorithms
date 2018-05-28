24cb25b4ca3265de813bed8f1e9ee4cd8b99fcd2
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2018-05-28 18:50:34 -0300

    black and grey

--- tads-tec.0017/assembly/megamania.asm
+++ tads-tec.0017/assembly/megamania.asm
@@ -1,2 +1,23 @@
 .text
 main:	lui $9, 0x1000 #$9 <= 0x10000000
+	lui $10, 0x0000 #preto
+	addi $11, $0, 11648
+
+fillpreto:  beq $11, $0, fimpreto
+              
+       	sw $10, 0($9)
+       	addi $9, $9, 4
+
+       	addi $11, $11, -1
+       	j fillpreto
+fimpreto: lui $10, 0x8080
+	ori $10, $10, 0x8080
+	addi $11, $0, 4096
+fillcinza: beq $11, $0, fimcinza
+
+	sw $10, 0($9)
+       	addi $9, $9, 4
+
+       	addi $11, $11, -1
+	j fillcinza
+fimcinza: nop
