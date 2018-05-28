7e317fe1c6083966305274606d61acea486eb775
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2018-05-28 18:59:22 -0300

    full bg

--- tads-tec.0017/assembly/megamania.asm
+++ tads-tec.0017/assembly/megamania.asm
@@ -14,10 +14,19 @@ fimpreto: lui $10, 0x8080
 	ori $10, $10, 0x8080
 	addi $11, $0, 4096
 fillcinza: beq $11, $0, fimcinza
-
 	sw $10, 0($9)
        	addi $9, $9, 4
 
        	addi $11, $11, -1
 	j fillcinza
-fimcinza: nop
+fimcinza: lui $10, 0x0000
+	addi $11, $0, 640
+fillpretorestante: beq $11, $0, fimpretorestante
+	sw $10, 0($9)
+	addi $9, $9, 4
+	
+	addi $11, $11, -1
+	j fillpretorestante
+	
+fimpretorestante: nop
+
