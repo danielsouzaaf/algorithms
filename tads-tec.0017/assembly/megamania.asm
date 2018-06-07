71a36c1de4f8cd34efc872e84359395b45fbcda3
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2018-06-07 16:56:53 -0300

    ahn

--- tads-tec.0017/assembly/megamania.asm
+++ tads-tec.0017/assembly/megamania.asm
@@ -1,7 +1,7 @@
 .text
 main:	lui $9, 0x1000 #$9 <= 0x10000000
 	lui $10, 0x0000 #preto
-	addi $11, $0, 11648
+	addi $11, $0, 11264
 
 fillpreto:  beq $11, $0, fimpreto
               
@@ -20,7 +20,7 @@ fillcinza: beq $11, $0, fimcinza
        	addi $11, $11, -1
 	j fillcinza
 fimcinza: lui $10, 0x0000
-	addi $11, $0, 640
+	addi $11, $0, 1024
 fillpretorestante: beq $11, $0, fimpretorestante
 	sw $10, 0($9)
 	addi $9, $9, 4
