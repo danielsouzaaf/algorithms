5ce3b1fe74714c2a3cf909de12f1e7087df0916a
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2018-06-14 14:02:51 -0300

    Update bitquantity.c

--- tads-tec.0015/recursive/bitquantity.c
+++ tads-tec.0015/recursive/bitquantity.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-long calculateQuantity(int n, int quantity)
+long calculateQuantity(int n)
 {
-	if (n == 0)
-		return quantity;
-	return (calculateQuantity(n/2, ++quantity));
+	if (n == 0) return 0;
+	if (n == 1) return 1;
+	return 1+calculateQuantity(n>>1);
 }
 
 int main()
 {
 	int n = 10;
-	int q = calculateQuantity(n, 0);
+	int q = calculateQuantity(n);
 	printf("%d\n", q);
-	
 	return 0;
 }
 
