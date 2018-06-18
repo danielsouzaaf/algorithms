1af3b81d6cb1b7fb94509d0c9412fd4d6157fc8b
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2018-06-18 15:55:38 -0300

    Update megamania_funcoes.asm

--- tads-tec.0017/assembly/megamania_funcoes.asm
+++ tads-tec.0017/assembly/megamania_funcoes.asm
@@ -142,7 +142,8 @@ desenharbarraenergia:
 	lui $10, 0x8080
 	ori $10, $10, 0x8080 #cinza
 	add $17, $0, $31
-
+	
+	#inicio desenha parte cinza
 	addi $9, $18, 21508
 	addi $12, $0, 508
 	jal pintar
@@ -186,7 +187,191 @@ desenharbarraenergia:
 	addi $9, $18, 26628
 	addi $12, $0, 508
 	jal pintar
+	#fim desenha parte cinza
 	
+	#inicio desenhar ENERGY
+	lui $10, 0x0000 #preto
+
+	#escrever E
+	addi $9, $18, 22568
+	addi $12, $0, 12
+	jal pintar
+	
+	addi $9, $18, 23080
+	addi $12, $0, 4
+	jal pintar
+
+	addi $9, $18, 23592
+	addi $12, $0, 12
+	jal pintar
+	
+	addi $9, $18, 24104
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 24616
+	addi $12, $0, 12
+	jal pintar
+	#fim escrever E
+	#escrever N
+	addi $9, $18, 22588
+	addi $12, $0, 4
+	jal pintar
+
+	addi $9, $18, 22600
+	addi $12, $0, 4
+	jal pintar
+		
+	addi $9, $18, 23100
+	addi $12, $0, 8
+	jal pintar
+
+	addi $9, $18, 23112
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 23592
+	addi $12, $0, 12
+	jal pintar
+	
+	addi $9, $18, 23612
+	addi $12, $0, 16
+	jal pintar
+	
+	addi $9, $18, 24124
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 24132
+	addi $12, $0, 8
+	jal pintar
+	
+	addi $9, $18, 24636
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 24648
+	addi $12, $0, 4
+	jal pintar
+	#fim escrever N
+	#escrever E
+	addi $9, $18, 22612
+	addi $12, $0, 12
+	jal pintar
+	
+	addi $9, $18, 23124
+	addi $12, $0, 4
+	jal pintar
+
+	addi $9, $18, 23636
+	addi $12, $0, 12
+	jal pintar
+	
+	addi $9, $18, 24148
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 24660
+	addi $12, $0, 12
+	jal pintar
+	#fim escrever E
+	#escrever R
+	addi $9, $18, 22636
+	addi $12, $0, 8
+	jal pintar
+	
+	addi $9, $18, 23148
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 23156
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 23660
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 23668
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 24172
+	addi $12, $0, 8
+	jal pintar
+	
+	addi $9, $18, 24684
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 24692
+	addi $12, $0, 4
+	jal pintar
+	#fim escrever R
+	#escrever G
+	addi $9, $18, 22660
+	addi $12, $0, 8
+	jal pintar
+	
+	addi $9, $18, 23172
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 23684
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 23692
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 24196
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 24204
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 24708
+	addi $12, $0, 12
+	jal pintar
+	#fim escrever G
+	#escrever Y
+	addi $9, $18, 22672
+	addi $12, $0, 8
+	jal pintar
+	
+	addi $9, $18, 22684
+	addi $12, $0, 8
+	jal pintar
+	
+	addi $9, $18, 23188
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 23196
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 23704
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 24216
+	addi $12, $0, 4
+	jal pintar
+	
+	addi $9, $18, 24728
+	addi $12, $0, 4
+	jal pintar
+	#fim escrever Y
+
+	#fim desenhar ENERGY
+	
+	#inicio desenhar vida
+	
+	#fim desenhar vida
 	
 	jr $17
 
