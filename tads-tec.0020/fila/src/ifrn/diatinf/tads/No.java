11ec26b7fe13ee5092f44f2287f74be670d78d2b
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2018-08-22 10:15:32 -0300

    Fila com Lista

---
+++ tads-tec.0020/fila/src/ifrn/diatinf/tads/No.java
@@ -0,0 +1,18 @@
+package ifrn.diatinf.tads;
+
+public class No {
+    private Object elemento;
+    private No proximo;
+
+    public Object getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Object o) {
+        elemento = o;
+    }
+
+    public void setProximo(No n) { proximo = n;}
+
+    public No getProximo() { return proximo; }
+}
